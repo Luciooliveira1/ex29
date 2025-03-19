45c794d398b14485e6c12f5bb1d54e8526792c32
LUCIO OLIVEIRA <jose.costa25@escola.pr.gov.br>
2025-03-19 11:05:29 -0300

    Add files via upload

--- luciooliveira/ex29.py
+++ luciooliveira/ex29.py
@@ -1,14 +1,47 @@
 def exibir_menu():
     print("Bem vindo ao menu de cadastro")
     print("1 - Novo cadastro")
-    print("Ver cadastro")
+    print("2 - Ver cadastro")
     print("3 - Sair")
     print("----------------------------------")
 
-    def cadastrar_pessoa (cadastros):
-        nome = input("nome:")
+def cadastrar_pessoa (cadastros):
+        nome = input("Nome:")
         idade = input("Idade:")
         turma = input("Turma:")
         curso = input("Curso:")
         cadastros.append({"Nome": nome, "idade": idade, "turma": turma, "cursos": curso,})
         print("Cadastro realizado com sucesso!")
+
+def ver_cadastros (cadastros):
+    if not cadastros:
+         print("Nenhum cadastro no sistema.")
+    else:
+        print("\n ----- LISTA DE CADASTROS -----")
+
+        for i, pessoa in enumerate (cadastros, 1):
+
+            print(f"{i}. nome: {pessoa['nome']}, Idade:  {pessoa['Idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Curso']}")
+
+
+def main():
+    cadastros = []
+    while True: 
+        exibir_menu()
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            cadastrar_pessoa(cadastros)
+        elif opcao == "2":
+           ver_cadastros(cadastros)  
+        elif opcao == "3":
+              print("obrigado por utilizar nosso sistemas!")
+        break
+    else:
+        print("opção incorreta! tente novamente.")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
